bde0fe09e205cac7725ae123c65d389aa6a6a444
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2024-06-13 23:50:39 +0530

    demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    a=12;
+    cout<<"Size of int: "<<sizeof(a)<<endl;
+    float b;
+    cout<<"Size of float: "<<sizeof(b)<<endl;
+    char c;
+    cout<<"Size of character: "<<sizeof(c)<<endl;
+    bool d;
+    cout<<"Size of boolean: "<<sizeof(d)<<endl;
+    short int e;
+    long int f;
+    cout<<"Size of short int: "<<sizeof(e)<<endl;
+    cout<<"Size of long int: "<<sizeof(f)<<endl;
+}
