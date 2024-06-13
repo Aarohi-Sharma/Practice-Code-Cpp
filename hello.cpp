b401607be748495b617f605f6f72d2fa07cf7961
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2024-06-13 23:52:52 +0530

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int32_t main(){
+    cout <<"hello world \n";
+    int a1;
+    cout<<"a1: ";
+    cin>>a1;
+    int a2;
+    cin>>a2;
+    int sum=a1+a2;
+    cout<<sum;
+}
