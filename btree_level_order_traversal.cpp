cbf7cf451ff0429208d40069634926482ade1714
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2024-11-13 21:42:26 +0530

    btree_level_order_traversal.cpp

---
+++ btree_level_order_traversal.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+void printLevelorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node* node=q.front();
+        q.pop();
+        if(node!=NULL){
+            cout<<node->data<<" ";
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){
+                q.push(node->right);
+            }
+        }
+        else if(!q.empty()){
+            q.push(NULL);
+        }
+    }
+}
+int sumAtK(Node* root, int K){
+    if (root==NULL){
+        return -1;
+    }
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    int level=0;
+    int sum=0;
+    while(!q.empty()){
+        Node* node =q.front();
+        q.pop();
+        if(node!=NULL){
+            if(level==K){
+                sum+=node->data;
+            }
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){
+                q.push(node->right);
+            }
+        }
+        else if(!q.empty()){
+            q.push(NULL);
+            level++;
+        }
+    }
+    return sum;
+}
+int main(){
+     struct Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    printLevelorder(root);
+    cout<<sumAtK(root, 2);
+}
